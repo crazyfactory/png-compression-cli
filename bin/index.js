1cbf5dfa420451bf1994b07e8707cfd6b7271584
wmathes <fanatik@gmx.at>
2017-06-07 14:07:41 +0700

    feat(pngc): initial release

---
+++ bin/index.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const execFile = require('child_process').execFile;
+const readChunk = require('read-chunk');
+const imageType = require('image-type');
+const zopfli = require('zopflipng-bin');
+const program = require('commander');
+const chalk = require('chalk');
+
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+
+updateNotifier({pkg}).notify();
+
+let files = [];
+
+function walk(path) {
+    const dirList = fs.readdirSync(path);
+    dirList.forEach(item => {
+        const stats = fs.statSync(path + '/' + item);
+        if (stats.isFile()) {
+            const imageTypeData = imageType(readChunk.sync(path + '/' + item, 0, 12));
+            if (imageTypeData) {
+                if (imageTypeData.ext === 'png') {
+                    files.push({
+                        file: path + '/' + item,
+                        size: stats.size
+                    });
+                }
+            }
+        }
+    });
+
+    if (program.recursive) {
+        dirList.forEach(item => {
+            if (fs.statSync(path + '/' + item).isDirectory()) {
+                walk(path + '/' + item);
+            }
+        });
+    }
+}
+
+program
+    .version(pkg.version || 'dev')
+    .option('-r, --recursive', 'Walk given directory recursively')
+    .option('-p, --path <path>', 'A path to crawl, defaults to current working directory')
+    .option('-f, --files <items>', 'the files you want to convert,split by \',\'', v => v.split(','))
+    .option('-m, --more', 'Compress harder, which will take more time')
+    .option('--lossy_transparent', 'Allow changing of fully transparent colors')
+    .option('--lossy_8bit', 'Reduce color channels from 16bit to 8bit')
+    .parse(process.argv);
+
+if (program.files) {
+    files = program.files.map(f => {
+        return {
+            file: f,
+            size: fs.statSync(f).size
+        };
+    });
+} else {
+    walk(process.path || process.cwd());
+}
+
+console.log(chalk.yellow('Found ' + files.length + ' image file(s) !'));
+
+files.forEach(item => {
+    const args = ['-y'];
+    if (program.more === true) {
+        args.push('-m');
+    }
+    if (program.lossyTransparent === true) {
+        args.push('--lossy_transparent');
+    }
+    if (program.lossy8bit === true) {
+        args.push('--lossy_8bit');
+    }
+    args.push(item.file, item.file);
+
+    execFile(zopfli, args, err => {
+        if (err) {
+            throw err;
+        }
+
+        item.newSize = fs.statSync(item.file).size;
+        const percDiff = Math.round(((1 - item.newSize / item.size) * 100));
+
+        if (percDiff > 0) {
+            console.log(chalk.green(item.file + ' was compressed by ' + percDiff + "%!"));
+        }
+        else {
+            console.log(chalk.gray(item.file + ' was already compressed'));
+        }
+    });
+});
---
+++ readme.md
@@ -0,0 +1,53 @@
+# jpeg-compression-cli
+
+[![npm](https://img.shields.io/npm/v/@crazyfactory/png-compression-cli.svg)](http://www.npmjs.com/package/@crazyfactory/png-compression-cli)
+[![Build Status](https://travis-ci.org/crazyfactory/png-compression-cli.svg?branch=master)](https://travis-ci.org/crazyfactory/png-compression-cli)
+[![dependencies Status](https://david-dm.org/crazyfactory/png-compression-cli/status.svg)](https://david-dm.org/crazyfactory/png-compression-cli)
+[![devDependencies Status](https://david-dm.org/crazyfactory/png-compression-cli/dev-status.svg)](https://david-dm.org/crazyfactory/png-compression-cli?type=dev)
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+[![Greenkeeper badge](https://badges.greenkeeper.io/crazyfactory/png-compression-cli.svg)](https://greenkeeper.io/)
+
+The CommandLine tool for converting jpg or png files to webp, recursively.
+
+## Awsome featurn ✧٩(ˊωˋ*)و✧
+This tool would search the jpg or png files by the head infomation with the files, so even the files without the ext name,they also would be found and be converted.
+
+so enjoy it !
+
+## Installation
+
+    $ npm i -g @crazyfactory/png-compression-cli
+
+## Usage
+
+png-compression-cli allow you to convert your images within a current directory, recursively or not. It will overwrite your files with the compressed version 
+
+After installation, just run command `pngc` in ternimal.
+
+To convert all images within the current directory and subdirectoies, use the -r flag
+
+    $ pngc -r
+    
+To choose a different folder provide it with -p flag
+
+    $ pngc -p /var/my-images
+
+To convert the specific image files (assets/img.jpg in this example), you may run the following command.
+
+    $ pngc -f assets/img.jpg
+    
+or
+
+    $ pngc -f assets/img1.jpg,assets/img2.png
+
+for more help infomation, you could run the -h flag to check it out.
+
+    $ pngc -h
+
+## License
+
+Copyright (c) 2017 Wolf T. Mathes for Crazy Factory Trading Co. Ltd.
+
+Licensed under the MIT license.
+
+See LICENSE for more info.
